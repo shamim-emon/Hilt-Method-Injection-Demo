fd9d94cbefd5cafde581de134bd9c650f884e570
shamim-emon <emon.dev.bd@gmail.com>
2025-01-20 05:23:45 +0600

    Initial Commit

---
+++ app/src/main/java/com/emon/hiltmethodinjectiondemo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.emon.hiltmethodinjectiondemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.emon.hiltmethodinjectiondemo.ui.theme.HiltMethodInjectionDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HiltMethodInjectionDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HiltMethodInjectionDemoTheme {
+        Greeting("Android")
+    }
+}
